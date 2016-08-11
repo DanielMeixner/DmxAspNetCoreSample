8b35591c5fab30e81394a4d326cd854e9ad6e8b6
Daniel Meixner <daniel.meixner@live.com>
2016-08-11 17:08:39 +0200

    all in one

--- Deploy/deploytoazure.ps1
+++ Deploy/deploytoazure.ps1
@@ -11,6 +11,26 @@ param($websiteName, $packOutput)
                  'Username'=$website.PublishingUsername;
                  'Password'=$website.PublishingPassword}
 
- $publishScript = "./default-publish.ps1"
+#  $publishScript = "./default-publish.ps1"
 
- $publishScript -publishProperties $publishProperties  -packOutput $packOutput
+#  . $publishScript -publishProperties $publishProperties  -packOutput $packOutput
+
+#  [cmdletbinding(SupportsShouldProcess=$true)]
+# param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = ''
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
